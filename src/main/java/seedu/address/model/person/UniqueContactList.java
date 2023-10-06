389eaaac1d652a7b2e89ec40d3f00fcdf8c6214d
CJ-Lee01 <chunjie01@live.com>
2023-10-06 22:24:48 +0800

    Fix style issue

--- src/main/java/seedu/address/model/person/UniqueContactList.java
+++ src/main/java/seedu/address/model/person/UniqueContactList.java
@@ -15,8 +15,8 @@ import seedu.address.model.person.exceptions.PersonNotFoundException;
  * A list of persons that enforces uniqueness between its elements and does not allow nulls.
  * A contact is considered unique by comparing using {@code Contact#isSamePerson(Contact)}. As such, adding and updating
  * of persons uses Contact#isSamePerson(Contact) for equality so as to ensure that the contact being added or updated is
- * unique in terms of identity in the UniqueContactList. However, the removal of a contact uses Contact#equals(Object) so
- * as to ensure that the contact with exactly the same fields will be removed.
+ * unique in terms of identity in the UniqueContactList. However, the removal of a contact uses Contact#equals(Object)
+ * so as to ensure that the contact with exactly the same fields will be removed.
  *
  * Supports a minimal set of list operations.
  *
