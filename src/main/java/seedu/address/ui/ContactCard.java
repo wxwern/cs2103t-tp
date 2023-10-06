498d6a3be19e0774f46f8ce07dd8909962adca17
CJ-Lee01 <chunjie01@live.com>
2023-10-06 22:17:13 +0800

    Update FXML file names

--- src/main/java/seedu/address/ui/ContactCard.java
+++ src/main/java/seedu/address/ui/ContactCard.java
@@ -14,7 +14,7 @@ import seedu.address.model.person.Contact;
  */
 public class ContactCard extends UiPart<Region> {
 
-    private static final String FXML = "PersonListCard.fxml";
+    private static final String FXML = "ContactListCard.fxml";
 
     /**
      * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
