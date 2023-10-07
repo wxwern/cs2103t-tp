2cecfa4ae4252d35e885772d3967f76e80e2de9c
Wern <dev@wern.cc>
2023-10-07 14:07:38 +0800

    Make flags use full names as per convention
    A future update will allow for prefix-only matching separately.
    For now, this is done to reduce ambiguity and allow for more flags to
    be defined with no conflicts.

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -5,11 +5,11 @@ package seedu.address.logic.parser;
  */
 public class CliSyntax {
 
-    /* Prefix definitions */
-    public static final Flag FLAG_NAME = new Flag("n");
-    public static final Flag FLAG_PHONE = new Flag("p");
-    public static final Flag FLAG_EMAIL = new Flag("e");
-    public static final Flag FLAG_ADDRESS = new Flag("a");
-    public static final Flag FLAG_TAG = new Flag("t");
+    /* Flag definitions */
+    public static final Flag FLAG_NAME = new Flag("name");
+    public static final Flag FLAG_PHONE = new Flag("phone");
+    public static final Flag FLAG_EMAIL = new Flag("email");
+    public static final Flag FLAG_ADDRESS = new Flag("addr");
+    public static final Flag FLAG_TAG = new Flag("tag");
 
 }
