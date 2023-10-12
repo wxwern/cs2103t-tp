4dfabe6efaff42daf59748949ef6912afdde1434
CJ-Lee01 <chunjie01@live.com>
2023-10-12 13:40:17 +0800

    Add functionality to delete child contacts

--- src/main/java/seedu/address/logic/commands/DeleteWithChildrenCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteWithChildrenCommand.java
@@ -1,16 +1,53 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+
+import java.util.Arrays;
+import java.util.List;
+
 import seedu.address.commons.core.index.Index;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.person.Contact;
 
 /**
  * Deletes a contact identified with its displayed index and also deletes its child contacts.
  */
 public class DeleteWithChildrenCommand extends DeleteCommand {
+
+    public static String MESSAGE_DELETE_CONTACT_SUCCESS = DeleteCommand.MESSAGE_DELETE_CONTACT_SUCCESS + "with:\n%s";
+
+    private final Index targetIndex;
+
     /**
      * @param targetIndex of the contact to be deleted in the current contact list
      */
     public DeleteWithChildrenCommand(Index targetIndex) {
         // TODO add documentation in DG
         super(targetIndex);
+        this.targetIndex = targetIndex;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Contact> lastShownList = model.getFilteredContactList();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_CONTACT_DISPLAYED_INDEX);
+        }
+
+        Contact contactToDelete = lastShownList.get(targetIndex.getZeroBased());
+        Contact[] childContacts = contactToDelete.getChildren();
+        model.deleteContact(contactToDelete);
+        Arrays.stream(childContacts).forEach(contact -> {model.deleteContact(contactToDelete);});
+        return new CommandResult(String.format(
+                MESSAGE_DELETE_CONTACT_SUCCESS,
+                Messages.format(contactToDelete),
+                Arrays.stream(childContacts)
+                        .map(c -> Messages.format(c) + "\n")
+                        .reduce((c1, c2) -> c1 + c2)
+        ));
     }
 }
