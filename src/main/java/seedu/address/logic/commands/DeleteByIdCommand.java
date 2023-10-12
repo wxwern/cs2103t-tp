5033786e19d37d453c7e3da9544704ada7a50410
CJ-Lee01 <chunjie01@live.com>
2023-10-12 13:02:26 +0800

    Add DeleteByIdCommand

---
+++ src/main/java/seedu/address/logic/commands/DeleteByIdCommand.java
@@ -0,0 +1,25 @@
+package seedu.address.logic.commands;
+
+import seedu.address.model.Model;
+import seedu.address.model.person.ContactId;
+
+/**
+ * Deletes a contact identified using its ID from the addressbook.
+ */
+public class DeleteByIdCommand extends DeleteCommand {
+
+    private final ContactId contactId;
+
+    public DeleteByIdCommand(ContactId id) {
+        // TODO: Might want to change this since null is dangerous
+        super(null);
+        this.contactId = id;
+    }
+
+    @Override
+    public CommandResult execute(Model model) {
+        // TODO: Implement the get function based on id for the model
+        // Currently overridden to avoid NPE
+        return null;
+    }
+}
