f4d5ddfd5a469f3310df2c2bfc8375fe019ed145
Tan Shi Yu <42184874+tanshiyu1999@users.noreply.github.com>
2023-10-15 12:34:15 +0000

    Update src/main/java/seedu/address/model/person/Position.java
    Co-authored-by: Wern <dev@wern.cc>

--- src/main/java/seedu/address/model/person/Position.java
+++ src/main/java/seedu/address/model/person/Position.java
@@ -15,7 +15,7 @@ public class Position {
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ][^(?![\\s\\S])]*";
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
 
     public final String jobPosition;
 
