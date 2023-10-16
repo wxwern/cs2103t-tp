ee662e9ab959a5b8d367588fbc9125c87cac1e2c
CJ-Lee01 <chunjie01@live.com>
2023-10-16 19:58:29 +0800

    Implement delete by id commands

--- src/main/java/seedu/address/logic/commands/DeleteByIdCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteByIdCommand.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import seedu.address.model.Model;
+import seedu.address.model.person.Contact;
 import seedu.address.model.person.ContactId;
 
 /**
@@ -8,6 +9,7 @@ import seedu.address.model.person.ContactId;
  */
 public class DeleteByIdCommand extends DeleteCommand {
 
+    public static final String NO_SUCH_CONTACT_WITH_ID = "No such contact with id: %s";
     private final ContactId contactId;
 
     /**
@@ -22,7 +24,11 @@ public class DeleteByIdCommand extends DeleteCommand {
     @Override
     public CommandResult execute(Model model) {
         // TODO: Implement the get function based on id for the model
-        // Currently overridden to avoid NPE
-        return null;
+        Contact c = model.getContactById(contactId);
+        if (c == null) {
+            return new CommandResult(String.format(NO_SUCH_CONTACT_WITH_ID, contactId));
+        }
+        model.deleteContact(c);
+        return new CommandResult(String.format(MESSAGE_DELETE_CONTACT_SUCCESS, c));
     }
 }
--- src/main/java/seedu/address/logic/commands/DeleteByIdWithChildrenCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteByIdWithChildrenCommand.java
@@ -1,16 +1,45 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.commands.DeleteByIdCommand.NO_SUCH_CONTACT_WITH_ID;
+
+import java.util.Arrays;
+
+import seedu.address.logic.Messages;
+import seedu.address.model.Model;
+import seedu.address.model.person.Contact;
 import seedu.address.model.person.ContactId;
 
 /**
  * Deletes a contact identified with its id and also deletes its child contacts.
  */
-public class DeleteByIdWithChildrenCommand extends DeleteByIdCommand {
+public class DeleteByIdWithChildrenCommand extends DeleteCommand {
 
+    private final ContactId contactId;
     /**
      * @param id of the contact to be deleted along with its child contacts.
      */
     public DeleteByIdWithChildrenCommand(ContactId id) {
-        super(id);
+        super(null);
+        this.contactId = id;
+    }
+
+    @Override
+    public CommandResult execute(Model model) {
+        Contact contactToDelete = model.getContactById(contactId);
+        if (contactToDelete == null) {
+            return new CommandResult(String.format(NO_SUCH_CONTACT_WITH_ID, contactId));
+        }
+        Contact[] childContacts = contactToDelete.getChildren();
+        model.deleteContact(contactToDelete);
+        Arrays.stream(childContacts).forEach(contact -> {
+            model.deleteContact(contactToDelete);
+        });
+        return new CommandResult(String.format(
+                DeleteWithChildrenCommand.MESSAGE_DELETE_CONTACT_SUCCESS,
+                Messages.format(contactToDelete),
+                Arrays.stream(childContacts)
+                        .map(c -> Messages.format(c) + "\n")
+                        .reduce((c1, c2) -> c1 + c2)
+        ));
     }
 }
