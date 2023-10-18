95877ecd17e19dd70be9095e7e2036ddf4564c88
Wern <dev@wern.cc>
2023-10-18 22:58:34 +0800

    Remove unused import

--- src/test/java/seedu/address/logic/parser/ArgumentMultimapTest.java
+++ src/test/java/seedu/address/logic/parser/ArgumentMultimapTest.java
@@ -3,7 +3,6 @@ package seedu.address.logic.parser;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
