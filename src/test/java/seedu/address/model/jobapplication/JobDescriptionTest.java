4bf25e0c573a6fdafa01b87ddad0c6a859668e0f
CJ-Lee01 <chunjie01@live.com>
2023-10-24 13:22:09 +0800

    Add more tests for JobDescription

--- src/test/java/seedu/address/model/jobapplication/JobDescriptionTest.java
+++ src/test/java/seedu/address/model/jobapplication/JobDescriptionTest.java
@@ -1,6 +1,8 @@
 package seedu.address.model.jobapplication;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
@@ -32,5 +34,21 @@ public class JobDescriptionTest {
         assertTrue(JobDescription.isValidJobDescription("Pay: $100")); // Special characters
         assertTrue(JobDescription.isValidJobDescription("To contact John for position"));
         assertTrue(JobDescription.isValidJobDescription("First apply, then wait.")); // More special char
+    }
+
+    @Test
+    public void equals() {
+        JobDescription jd1 = new JobDescription("Pay: $100");
+        JobDescription jd2 = new JobDescription("Pay: $100");
+        JobDescription jd3 = new JobDescription("Contact John");
+
+        // self -> true
+        assertEquals(jd1, jd1);
+
+        // same description -> true
+        assertEquals(jd1, jd2);
+
+        // different description -> false
+        assertNotEquals(jd1, jd3);
     }
 }
