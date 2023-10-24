d4864e37b66bfdf5bf38ae4839c14fafb18997e3
CJ-Lee01 <chunjie01@live.com>
2023-10-24 16:28:54 +0800

    Give skeletal implementation for Deadline

--- src/main/java/seedu/address/model/jobapplication/Deadline.java
+++ src/main/java/seedu/address/model/jobapplication/Deadline.java
@@ -1,8 +1,49 @@
 package seedu.address.model.jobapplication;
 
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
 /**
  * Represents the various job application deadlines.
  * Such as deadlines for applications, deadlines for online assessments.
  */
 public class Deadline {
+
+    public static final String MESSAGE_CONSTRAINTS =
+            "";
+
+    public static final String VALIDATION_REGEX = "";
+    public final LocalDate deadline;
+
+    public Deadline(String deadline) {
+        requireNonNull(deadline);
+        checkArgument(isValidDeadline(deadline), MESSAGE_CONSTRAINTS);
+        this.deadline = LocalDate.parse(deadline);
+    }
+
+    public Deadline() {
+        this.deadline = LocalDate.now().plusDays(14);
+    }
+
+    public static boolean isValidDeadline(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+
+        if (!(other instanceof Deadline)) {
+            return false;
+        }
+
+        Deadline otherDeadline = (Deadline) other;
+        return deadline.equals(otherDeadline.deadline)
+    }
 }
