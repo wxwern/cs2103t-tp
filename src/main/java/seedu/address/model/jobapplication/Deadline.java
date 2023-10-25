8a7465665f74b366f9488575d068075fd9cf6b91
CJ-Lee01 <chunjie01@live.com>
2023-10-25 16:52:13 +0800

    Remove unused import

--- src/main/java/seedu/address/model/jobapplication/Deadline.java
+++ src/main/java/seedu/address/model/jobapplication/Deadline.java
@@ -6,7 +6,6 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Formatter;
 
 /**
  * Represents the various job application deadlines.
