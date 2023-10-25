e077fc1997d38a62ddfe3663b6ea94f38d7a4ca7
CJ-Lee01 <chunjie01@live.com>
2023-10-25 22:08:30 +0800

    Implement new model method in inner class

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -16,6 +16,7 @@ import org.junit.jupiter.api.Test;
 
 import javafx.collections.ObservableList;
 import seedu.address.commons.core.GuiSettings;
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.AddressBook;
@@ -151,6 +152,11 @@ public class AddCommandTest {
 
         @Override
         public Contact getContactById(Id id) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public Contact getContactByIdXorIndex(Id id, Index index) {
             throw new AssertionError("This method should not be called.");
         }
 
