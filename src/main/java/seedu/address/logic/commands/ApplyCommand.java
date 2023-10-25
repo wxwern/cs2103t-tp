82983bd89a2a6769ecceb11bc0a8051ae071aab3
CJ-Lee01 <chunjie01@live.com>
2023-10-25 20:18:42 +0800

    Add skeletal ApplyCommand class

---
+++ src/main/java/seedu/address/logic/commands/ApplyCommand.java
@@ -0,0 +1,8 @@
+package seedu.address.logic.commands;
+
+/**
+ * Adds a job application to the address book.
+ */
+public class ApplyCommand {
+    public static final String COMMAND_WORD = "apply";
+}
