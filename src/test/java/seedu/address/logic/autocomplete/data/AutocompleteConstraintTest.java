7638f7fe616149d8aac87028c3f45741b6805f58
Wern <dev@wern.cc>
2023-10-26 18:50:08 +0800

    Improve AutocompleteConstraintTest

--- src/test/java/seedu/address/logic/autocomplete/data/AutocompleteConstraintTest.java
+++ src/test/java/seedu/address/logic/autocomplete/data/AutocompleteConstraintTest.java
@@ -11,91 +11,60 @@ public class AutocompleteConstraintTest {
 
     @Test
     public void onceForEachOf() {
+        var oneTwoThreeOnlyOnce = AutocompleteConstraint.onceForEachOf(1, 2, 3);
 
         // Allowed if the single arg itself is not yet added
-        assertTrue(AutocompleteConstraint.onceForEachOf(1, 2, 3)
-                .isAllowed(0, Set.of(2, 3)));
-
-        assertTrue(AutocompleteConstraint.onceForEachOf(1, 2, 3)
-                .isAllowed(1, Set.of(2, 3)));
+        assertTrue(oneTwoThreeOnlyOnce.isAllowed(0, Set.of(2, 3)));
+        assertTrue(oneTwoThreeOnlyOnce.isAllowed(1, Set.of(2, 3)));
 
         // Disallowed if the single arg itself is already present
-        assertFalse(AutocompleteConstraint.onceForEachOf(1, 2, 3)
-                .isAllowed(3, Set.of(2, 3)));
+        assertFalse(oneTwoThreeOnlyOnce.isAllowed(3, Set.of(2, 3)));
 
         // Unspecified elements are not impacted and allowed
-        assertTrue(AutocompleteConstraint.onceForEachOf(1, 2, 3)
-                .isAllowed(0, Set.of()));
-
-        assertTrue(AutocompleteConstraint.onceForEachOf(1, 2, 3)
-                .isAllowed(0, Set.of(0)));
-
-        assertTrue(AutocompleteConstraint.onceForEachOf(1, 2, 3)
-                .isAllowed(0, Set.of(1, 2)));
+        assertTrue(oneTwoThreeOnlyOnce.isAllowed(0, Set.of()));
+        assertTrue(oneTwoThreeOnlyOnce.isAllowed(0, Set.of(0)));
+        assertTrue(oneTwoThreeOnlyOnce.isAllowed(0, Set.of(1, 2)));
     }
 
     @Test
     public void oneAmongAllOf() {
+        var oneXorTwo = AutocompleteConstraint.oneAmongAllOf(1, 2);
+
         // Allowed if none of the arguments are added yet
-        assertTrue(AutocompleteConstraint.oneAmongAllOf(1, 2)
-                .isAllowed(1, Set.of(3)));
+        assertTrue(oneXorTwo.isAllowed(1, Set.of(3)));
 
         // Disallowed if at least one of the arguments is already present
-        assertFalse(AutocompleteConstraint.oneAmongAllOf(1, 2)
-                .isAllowed(1, Set.of(2, 3)));
-
-        assertFalse(AutocompleteConstraint.oneAmongAllOf(1, 2)
-                .isAllowed(2, Set.of(2, 3)));
+        assertFalse(oneXorTwo.isAllowed(1, Set.of(2, 3)));
+        assertFalse(oneXorTwo.isAllowed(2, Set.of(2, 3)));
 
         // Unspecified elements are not impacted and allowed
-        assertTrue(AutocompleteConstraint.oneAmongAllOf(1, 2)
-                .isAllowed(0, Set.of()));
-
-        assertTrue(AutocompleteConstraint.oneAmongAllOf(1, 2)
-                .isAllowed(0, Set.of(0)));
-
-        assertTrue(AutocompleteConstraint.oneAmongAllOf(1, 2)
-                .isAllowed(0, Set.of(2, 3)));
+        assertTrue(oneXorTwo.isAllowed(0, Set.of()));
+        assertTrue(oneXorTwo.isAllowed(0, Set.of(0)));
+        assertTrue(oneXorTwo.isAllowed(0, Set.of(2, 3)));
     }
 
     @Test
     public void isPrerequisiteFor() {
+        var oneIsPrereqForTwoAndThree = AutocompleteConstraint.where(1).isPrerequisiteFor(2, 3);
+
         // Prerequisite is always allowed
-        assertTrue(AutocompleteConstraint.where(1).isPrerequisiteFor(2, 3)
-                .isAllowed(1, Set.of()));
-
-        assertTrue(AutocompleteConstraint.where(1).isPrerequisiteFor(2, 3)
-                .isAllowed(1, Set.of(0)));
-
-        assertTrue(AutocompleteConstraint.where(1).isPrerequisiteFor(2, 3)
-                .isAllowed(1, Set.of(0, 1)));
+        assertTrue(oneIsPrereqForTwoAndThree.isAllowed(1, Set.of()));
+        assertTrue(oneIsPrereqForTwoAndThree.isAllowed(1, Set.of(0)));
+        assertTrue(oneIsPrereqForTwoAndThree.isAllowed(1, Set.of(0, 1)));
 
         // Dependents are conditionally allowed
-        assertTrue(AutocompleteConstraint.where(1).isPrerequisiteFor(2, 3)
-                .isAllowed(2, Set.of(1)));
-
-        assertTrue(AutocompleteConstraint.where(1).isPrerequisiteFor(2, 3)
-                .isAllowed(2, Set.of(1, 2, 3)));
-
-        assertFalse(AutocompleteConstraint.where(1).isPrerequisiteFor(2, 3)
-                .isAllowed(2, Set.of(3)));
+        assertTrue(oneIsPrereqForTwoAndThree.isAllowed(2, Set.of(1)));
+        assertTrue(oneIsPrereqForTwoAndThree.isAllowed(2, Set.of(1, 2, 3)));
+        assertFalse(oneIsPrereqForTwoAndThree.isAllowed(2, Set.of(3)));
 
         // Existing dependents don't impact prerequisites
-        assertTrue(AutocompleteConstraint.where(1).isPrerequisiteFor(2, 3)
-                .isAllowed(1, Set.of(2, 3)));
+        assertTrue(oneIsPrereqForTwoAndThree.isAllowed(1, Set.of(2, 3)));
 
         // Unspecified elements are not impacted and allowed
-        assertTrue(AutocompleteConstraint.where(1).isPrerequisiteFor(2, 3)
-                .isAllowed(0, Set.of()));
-
-        assertTrue(AutocompleteConstraint.where(1).isPrerequisiteFor(2, 3)
-                .isAllowed(0, Set.of(0)));
-
-        assertTrue(AutocompleteConstraint.where(1).isPrerequisiteFor(2, 3)
-                .isAllowed(0, Set.of(1)));
-
-        assertTrue(AutocompleteConstraint.where(1).isPrerequisiteFor(2, 3)
-                .isAllowed(0, Set.of(2, 3)));
+        assertTrue(oneIsPrereqForTwoAndThree.isAllowed(0, Set.of()));
+        assertTrue(oneIsPrereqForTwoAndThree.isAllowed(0, Set.of(0)));
+        assertTrue(oneIsPrereqForTwoAndThree.isAllowed(0, Set.of(1)));
+        assertTrue(oneIsPrereqForTwoAndThree.isAllowed(0, Set.of(2, 3)));
     }
 
 }
