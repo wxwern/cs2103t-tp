6685f5e698ba41f51f9bef8fe8649c95f64f002a
CJ-Lee01 <chunjie01@live.com>
2023-10-26 14:57:41 +0800

    Add test method to to ApplyCommandTest

--- src/test/java/seedu/address/logic/commands/ApplyCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ApplyCommandTest.java
@@ -1,4 +1,12 @@
 package seedu.address.logic.commands;
 
+import org.junit.jupiter.api.Test;
+
+
 public class ApplyCommandTest {
+
+    @Test
+    public void execute_nullIdAndIndex_throwsException() {
+        // assertCommandFailure(new ApplyCommand(null, null, null, null, null, null, null), );
+    }
 }
