0e014c663fefa906467296f5c5b54911eab4d628
CJ-Lee01 <chunjie01@live.com>
2023-10-27 18:50:58 +0800

    Fix import issue

--- src/main/java/seedu/address/storage/JsonAdaptedApplication.java
+++ src/main/java/seedu/address/storage/JsonAdaptedApplication.java
@@ -6,9 +6,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import seedu.address.commons.exceptions.IllegalValueException;
-
 import seedu.address.model.contact.Id;
-
 import seedu.address.model.jobapplication.ApplicationStage;
 import seedu.address.model.jobapplication.Deadline;
 import seedu.address.model.jobapplication.JobApplication;
