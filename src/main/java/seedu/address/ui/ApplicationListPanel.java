46a14d01be452547f0478a8622341289b14be900
CJ-Lee01 <chunjie01@live.com>
2023-10-28 18:10:49 +0800

    Add ui java files for JobApplication

---
+++ src/main/java/seedu/address/ui/ApplicationListPanel.java
@@ -0,0 +1,48 @@
+package seedu.address.ui;
+
+import java.util.logging.Logger;
+
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.Region;
+
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.model.jobapplication.JobApplication;
+
+public class ApplicationListPanel extends UiPart<Region> {
+    private static String FXML = "ApplicationListPanel.fxml";
+    private final Logger logger = LogsCenter.getLogger(ApplicationListPanel.class);
+
+    @FXML
+    private ListView<JobApplication> applicationListView;
+
+    /**
+     * Creates a {@code ContactListPanel} with the given {@code ObservableList}.
+     */
+    public ApplicationListPanel(ObservableList<JobApplication> applicationList) {
+        super(FXML);
+        applicationListView.setItems(applicationList);
+        applicationListView.setCellFactory(listView -> new ApplicationListViewCell());
+    }
+
+    /**
+     * Custom {@code ListCell} that displays the graphics of a {@code JobApplication} using a {@code ApplicationCard}.
+     * Same as {@link seedu.address.ui.ContactListPanel.ContactListViewCell}
+     */
+    class ApplicationListViewCell extends ListCell<JobApplication> {
+        @Override
+        protected void updateItem(JobApplication application, boolean empty) {
+            super.updateItem(application, empty);
+
+            if (empty || application == null) {
+                setGraphic(null);
+                setText(null);
+            } else {
+                setGraphic(new ApplicationCard(application, getIndex() + 1).getRoot());
+            }
+        }
+    }
+
+}
