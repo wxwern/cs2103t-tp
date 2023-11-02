b7bcf35ab3d0fa4ca83da4239522f74906477c39
McNaBry <mcnabry123@gmail.com>
2023-11-03 02:47:47 +0800

    Fix checkstyle

--- src/test/java/seedu/address/model/jobapplication/JobApplicationTest.java
+++ src/test/java/seedu/address/model/jobapplication/JobApplicationTest.java
@@ -5,15 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_ID_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
 import static seedu.address.testutil.Assert.assertThrows;
-import static seedu.address.testutil.TypicalContacts.ALICE;
-import static seedu.address.testutil.TypicalContacts.BOB;
 import static seedu.address.testutil.TypicalContacts.NTU;
 import static seedu.address.testutil.TypicalContacts.NUS;
 
@@ -21,9 +13,7 @@ import java.util.Objects;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.model.contact.Contact;
 import seedu.address.model.contact.Id;
-import seedu.address.testutil.ContactBuilder;
 
 public class JobApplicationTest {
     public static final JobApplication SAMPLE_JOB_APPLICATION = new JobApplication(
