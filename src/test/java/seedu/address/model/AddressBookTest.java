25e13da9a1c4aa46613b57dd03de48a1ed68e1b2
McNaBry <mcnabry123@gmail.com>
2023-11-02 15:40:21 +0800

    Update AddressBookTest

--- src/test/java/seedu/address/model/AddressBookTest.java
+++ src/test/java/seedu/address/model/AddressBookTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_ID_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalContacts.ALICE;
@@ -71,10 +72,17 @@ public class AddressBookTest {
     }
 
     @Test
-    public void hasContact_contactWithSameIdentityFieldsInAddressBook_returnsFalse() {
+    public void hasContact_contactWithSameIdInAddressBook_returnsTrue() {
         addressBook.addContact(ALICE);
-        Contact editedAlice = new ContactBuilder(ALICE).withAddress(VALID_ADDRESS_BOB).withTags(VALID_TAG_HUSBAND)
-                .build();
+        Contact editedAlice = new ContactBuilder(ALICE).withAddress(VALID_ADDRESS_BOB)
+                .withTags(VALID_TAG_HUSBAND).build();
+        assertTrue(addressBook.hasContact(editedAlice));
+    }
+
+    @Test
+    public void hasContact_contactWithDifferentIdInAddressBook_returnsFalse() {
+        addressBook.addContact(ALICE);
+        Contact editedAlice = new ContactBuilder(ALICE).withId(VALID_ID_BOB).build();
         assertFalse(addressBook.hasContact(editedAlice));
     }
 
