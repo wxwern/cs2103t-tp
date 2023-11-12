c1b8c5bbe48d065ecf43fe4064f9bb61990cd015
CJ-Lee01 <chunjie01@live.com>
2023-11-12 23:29:13 +0800

    Add more tests

--- src/test/java/seedu/address/logic/commands/ApplyCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ApplyCommandTest.java
@@ -1,17 +1,24 @@
 package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalContacts.NUS;
+import static seedu.address.testutil.TypicalContacts.RYAN;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
+import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
 import seedu.address.model.contact.Id;
+import seedu.address.model.jobapplication.JobApplication;
 import seedu.address.model.jobapplication.JobTitle;
+import seedu.address.testutil.TypicalContacts;
 
 
 public class ApplyCommandTest {
@@ -55,6 +62,7 @@ public class ApplyCommandTest {
     @Test
     public void execute_noSuchId_throwsException() {
         Model model = new ModelManager();
+
         assertThrows(
                 CommandException.class, () -> new ApplyCommand(
                         new Id(), null, new JobTitle("SWE"),
@@ -66,14 +74,15 @@ public class ApplyCommandTest {
     @Test
     public void execute_applyToNonOrg_throwsException() {
         Model model = new ModelManager();
-        model.addContact(NUS);
-        assertThrows(
-                CommandException.class, () -> new ApplyCommand(
-                        null, Index.fromOneBased(1), new JobTitle("SWE"),
-                        null, null, null,
-                        null)
-                        .execute(model)
-        );
+        model.addContact(RYAN);
+
+        String expectedMessage = String.format(ApplyCommand.MESSAGE_ATTEMPT_TO_ADD_TO_NON_ORG, RYAN);
+
+        ApplyCommand command = new ApplyCommand(
+                null, Index.fromOneBased(1), new JobTitle("SWE"),
+                null, null, null, null);
+
+        assertCommandFailure(command, model, expectedMessage);
     }
 
     @Test
@@ -92,11 +101,41 @@ public class ApplyCommandTest {
     public void execute_validApplication_doesNotThrowException() {
         Model model = new ModelManager();
         model.addContact(NUS);
+
+        JobTitle uniqueJobTitle = new JobTitle("This is a unique job title");
+
+        JobApplication expectedApplication = new JobApplication(NUS, uniqueJobTitle,
+                null, null, null);
+
+        ApplyCommand command = new ApplyCommand(null, Index.fromOneBased(1), uniqueJobTitle,
+                null, null, null, null);
+
+        Model expectedModel = new ModelManager();
+        expectedModel.addContact(NUS);
+        expectedModel.addApplication(expectedApplication);
+
+        String expectedMessage = String.format(ApplyCommand.MESSAGE_APPLY_SUCCESS, expectedApplication, NUS);
+
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+
         assertDoesNotThrow(() -> new ApplyCommand(
-                        null, Index.fromOneBased(1), new JobTitle("SWE"),
+                        null, Index.fromOneBased(1), new JobTitle("This is a unique position"),
                         null, null, null,
                         null)
                         .execute(model)
         );
+    }
+
+    @Test
+    public void execute_applicationWithSameName_throwException() {
+        JobTitle title = NUS.getJobApplications()[0].getJobTitle();
+        Model model = new ModelManager();
+        model.addContact(NUS);
+
+        ApplyCommand command = new ApplyCommand(
+                null, Index.fromOneBased(1), title,
+                null, null, null, null);
+
+        assertCommandFailure(command, model, ApplyCommand.MESSAGE_DUPLICATE_APPLICATION);
     }
 }
--- src/test/java/seedu/address/logic/commands/EditApplicationCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditApplicationCommandTest.java
@@ -1,5 +1,110 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.testutil.TypicalContacts.NUS;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.Messages;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.jobapplication.JobApplication;
+import seedu.address.model.jobapplication.JobTitle;
+import seedu.address.testutil.TypicalContacts;
+
 class EditApplicationCommandTest {
 
+    private Model model = new ModelManager(TypicalContacts.getTypicalAddressBook(), new UserPrefs());
+
+    private JobTitle uniqueTitle = new JobTitle("Unique title");
+    @Test
+    public void execute_validIndex_editSuccessfully() {
+
+        EditApplicationCommand.EditApplicationDescriptor editApplicationDescriptor =
+                new EditApplicationCommand.EditApplicationDescriptor();
+
+        Model model = new ModelManager(TypicalContacts.getTypicalAddressBook(), new UserPrefs());
+
+        editApplicationDescriptor.setJobTitle(uniqueTitle);
+
+        EditApplicationCommand command = new EditApplicationCommand(Index.fromOneBased(1), editApplicationDescriptor);
+
+        assertDoesNotThrow(() -> command.execute(model));
+
+        JobApplication firstApplication = model.getDisplayedApplicationList().get(0);
+
+        assertEquals(uniqueTitle, firstApplication.getJobTitle());
+    }
+
+    @Test
+    public void execute_invalidIndex_throwsException() {
+
+        EditApplicationCommand.EditApplicationDescriptor editApplicationDescriptor =
+                new EditApplicationCommand.EditApplicationDescriptor();
+
+        Model model = new ModelManager(TypicalContacts.getTypicalAddressBook(), new UserPrefs());
+
+        editApplicationDescriptor.setJobTitle(uniqueTitle);
+
+        EditApplicationCommand command = new EditApplicationCommand(Index.fromOneBased(20), editApplicationDescriptor);
+
+        assertCommandFailure(command, model, Messages.MESSAGE_INVALID_APPLICATION_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void execute_emptyFields_throwsException() {
+
+        EditApplicationCommand.EditApplicationDescriptor editApplicationDescriptor =
+                new EditApplicationCommand.EditApplicationDescriptor();
+
+        Model model = new ModelManager(TypicalContacts.getTypicalAddressBook(), new UserPrefs());
+
+        EditApplicationCommand command = new EditApplicationCommand(Index.fromOneBased(20), editApplicationDescriptor);
+
+        assertCommandFailure(command, model, EditApplicationCommand.MESSAGE_NOT_EDITED);
+    }
+
+    @Test
+    public void equals() {
+        EditApplicationCommand.EditApplicationDescriptor editApplicationDescriptorOne =
+                new EditApplicationCommand.EditApplicationDescriptor();
+
+        EditApplicationCommand.EditApplicationDescriptor editApplicationDescriptorTwo =
+                new EditApplicationCommand.EditApplicationDescriptor();
+
+        editApplicationDescriptorOne.setJobTitle(uniqueTitle);
+
+        Index one = Index.fromOneBased(1);
+
+        Index two = Index.fromOneBased(2);
+
+        EditApplicationCommand commandOne = new EditApplicationCommand(one, editApplicationDescriptorOne);
+        EditApplicationCommand commandTwo = new EditApplicationCommand(one, editApplicationDescriptorOne);
+
+        // different index
+        EditApplicationCommand commandThree = new EditApplicationCommand(two, editApplicationDescriptorOne);
+
+        // different descriptor
+        EditApplicationCommand commandFour = new EditApplicationCommand(one, editApplicationDescriptorTwo);
+
+        // itself
+        assertEquals(commandOne, commandOne);
+
+        // same index and descriptor
+        assertEquals(commandOne, commandTwo);
+
+        // different index
+        assertNotEquals(commandOne, commandThree);
+
+        // different descriptor
+        assertNotEquals(commandOne, commandFour);
+
+        // null
+        assertNotEquals(commandOne, null);
+    }
 }
