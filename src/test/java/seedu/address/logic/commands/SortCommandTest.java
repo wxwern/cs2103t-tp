59f8e7b7f73fd0ea77e20a127bf54b8662e494bd
LAPTOP-NQHH6Q7K\Juanpa <juanpaabola@gmail.com>
2023-11-14 01:47:44 +0800

    Add test cases for List, Sort, and Reminder

--- src/test/java/seedu/address/logic/commands/SortCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SortCommandTest.java
@@ -2,17 +2,25 @@ package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalContacts.getTypicalAddressBook;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
 import seedu.address.model.contact.Contact;
 import seedu.address.model.jobapplication.JobApplication;
 
 class SortCommandTest {
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    private Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    private Model unsortedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
-    // TODO: tech debt - lousy tests
     @Test
     public void equals() {
         Comparator<Contact> a = (c, b) -> c.getType().compareTo(b.getType());
@@ -33,5 +41,41 @@ class SortCommandTest {
         assertNotEquals(s, new SortCommand(a, j2, false));
         assertNotEquals(s, new SortCommand(d, j, false));
         assertNotEquals(s, new SortCommand(a, j, true));
+    }
+
+    @Test
+    public void execute_sortName_contactsSorted() {
+        String expectedMessage = SortCommand.MESSAGE_SORTED_CONTACTS;
+        SortCommand command = new SortCommand(Model.COMPARATOR_NAME, null, false);
+        expectedModel.updateSortedContactList(Model.COMPARATOR_NAME);
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        ArrayList<Contact> arrayList1 = new ArrayList<>(model.getDisplayedContactList());
+        ArrayList<Contact> arrayList2 = new ArrayList<>(expectedModel.getDisplayedContactList());
+        assertEquals(arrayList1, arrayList2);
+    }
+
+    @Test
+    public void execute_sortTitle_applicationsSorted() {
+        String expectedMessage = SortCommand.MESSAGE_SORTED_APPLICATIONS;
+        SortCommand command = new SortCommand(null, JobApplication.JOB_TITLE_COMPARATOR,
+                false);
+        expectedModel.updateSortedApplicationList(JobApplication.JOB_TITLE_COMPARATOR);
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        ArrayList<JobApplication> arrayList1 = new ArrayList<>(model.getDisplayedApplicationList());
+        ArrayList<JobApplication> arrayList2 = new ArrayList<>(expectedModel.getDisplayedApplicationList());
+        assertEquals(arrayList1, arrayList2);
+    }
+
+    @Test
+    public void execute_sortNone_applicationsUnsorted() {
+        String expectedMessage = SortCommand.MESSAGE_RESET_SORTING;
+        SortCommand command = new SortCommand(null, null, true);
+        assertCommandSuccess(command, model, expectedMessage, unsortedModel);
+        ArrayList<Contact> arrayListContact1 = new ArrayList<>(model.getDisplayedContactList());
+        ArrayList<Contact> arrayListContact2 = new ArrayList<>(unsortedModel.getDisplayedContactList());
+        ArrayList<JobApplication> arrayListApp1 = new ArrayList<>(model.getDisplayedApplicationList());
+        ArrayList<JobApplication> arrayListApp2 = new ArrayList<>(unsortedModel.getDisplayedApplicationList());
+        assertEquals(arrayListApp1, arrayListApp2);
+        assertEquals(arrayListContact1, arrayListContact2);
     }
 }
--- src/test/java/seedu/address/logic/parser/ListCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ListCommandParserTest.java
@@ -2,6 +2,9 @@ package seedu.address.logic.parser;
 
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_CONTACTS;
+import static seedu.address.model.Model.PREDICATE_SHOW_NOT_APPLIED_ORGANIZATIONS;
+import static seedu.address.model.Model.PREDICATE_SHOW_ONLY_ORGANIZATIONS;
+import static seedu.address.model.Model.PREDICATE_SHOW_ONLY_RECRUITERS;
 
 import org.junit.jupiter.api.Test;
 
@@ -14,5 +17,8 @@ public class ListCommandParserTest {
     @Test
     public void parse_validArgs_returnsListCommand() {
         assertParseSuccess(parser, "", new ListCommand(PREDICATE_SHOW_ALL_CONTACTS));
+        assertParseSuccess(parser, "--org", new ListCommand(PREDICATE_SHOW_ONLY_ORGANIZATIONS));
+        assertParseSuccess(parser, "--rec", new ListCommand(PREDICATE_SHOW_ONLY_RECRUITERS));
+        assertParseSuccess(parser, "--toapply", new ListCommand(PREDICATE_SHOW_NOT_APPLIED_ORGANIZATIONS));
     }
 }
